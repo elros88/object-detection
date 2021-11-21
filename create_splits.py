761b0dfd8ca3c387b7171fd04f926c6bae20f1e0
Antonio Ortiz <antonio.ortiz.988@gmail.com>
2021-11-21 07:38:40 +0000

    [feat] Implemented Data Splits

--- create_splits.py
+++ create_splits.py
@@ -20,6 +20,41 @@ def split(data_dir):
     # TODO: Split the data present in `/home/workspace/data/waymo/training_and_validation` into train and val sets.
     # You should move the files rather than copy because of space limitations in the workspace.
 
+    files = []
+
+    for file in os.listdir(data_dir):
+        files.append(file)
+
+    random.shuffle(files)
+    train = 100 * 75 / len(files)
+    val = len(files) - (100 * 10 / len(files))
+    test = len(files)
+
+    train_path = data_dir + "../train/"
+    val_path = data_dir + "../val/"
+    test_path = data_dir + "../test/"
+
+    file_counter = 0
+
+    for file in files:
+        source_file = data_dir + file
+        dest_file = ""
+
+        if file_counter < train:
+            dest_file = train_path + file
+
+        if train <= file_counter and file_counter < val:
+            dest_file = val_path + file
+
+
+        if val <= file_counter < test:
+            dest_file = test_path + file
+
+        os.popen("cp {} {}".format(source_file, dest_file))
+
+        file_counter += 1
+    
+
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
     parser.add_argument('--data_dir', required=True,
